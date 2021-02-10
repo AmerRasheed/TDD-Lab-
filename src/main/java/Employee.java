e0f5ffca9525814d60262b989f0f6f85435a2b2a
Amer <amer.rasheed@gmail.com>
2021-02-11 00:35:27 +0100

    TDD Lab- Day 1

---
+++ src/main/java/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+
+    public Employee(int i,
+                    String test_first_name,
+                    String test_last_name,
+                    int Age) {
+    }
+}
---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,18 @@
+//Each employee must have the following information:
+//First name and last name. (String)
+//Age. (int)
+//A company ID that is unique to each employee. This number must be generated
+//automatically when an employee is created in the system. (int)
+//A monthly salary. (double / float)
+//It must be possible to retrieve and set up all of the above information from an employee.
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+   // private Employee testObjectEmployee;
+    @Test
+    void testEmployee() {
+ Employee testObjectEmployee = new Employee(1,"Test First Name", "Test Last Name", 34);
+
+    }
+}
